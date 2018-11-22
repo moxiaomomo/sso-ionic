5dd86921544a2e931347450e0c87edfe448d3042
moxiaomomo <moxiaomomo@gmail.com>
2018-11-22 16:22:14 +0800

    Update logout logic

---
+++ src/app/home/service/backButton.service.ts
@@ -0,0 +1,7 @@
+import {Injectable} from '@angular/core';
+import {Platform, ToastController, App, NavController, Tabs} from '@ionic/angular';
+import {AppMinimize} from "@ionic-native/app-minimize";
+
+@Injectable()
+export class BackButtonService {
+}
